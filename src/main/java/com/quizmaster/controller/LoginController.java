df97594cd3f8caa06ed405913d7c71efbc47cf98
fuad <ahsanfuad@gmail.com>
2018-05-08 17:29:12 +0600

    completed login and signup with database..SHA-256 with SALT is used for password encryption

--- src/main/java/com/quizmaster/controller/LoginController.java
+++ src/main/java/com/quizmaster/controller/LoginController.java
@@ -1,15 +1,13 @@
 package com.quizmaster.controller;
 
 import com.quizmaster.database.DBConnection;
+import com.quizmaster.encryption.Hashing;
 import com.quizmaster.model.Login;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 @ManagedBean(name = "logincontroller")
 @RequestScoped
@@ -29,22 +27,72 @@ public class LoginController {
     }
 
     public void performLogIn(){
-        Connection connection = null;
 
-        DBConnection dbConnection = new DBConnection();
-        connection = dbConnection.getConnection();
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from qmusers");
-            System.out.println("...............");
-            while (resultSet.next()){
-                System.out.println(resultSet.getString(1)+" "+resultSet.getString(2)+" ");
+        String salt = userAvailable();
+        if(!salt.isEmpty()){
+            Hashing hashing = new Hashing();
+            String checkHashedPassword = hashing.performHash(login.getUserPassword(), salt);
+            DBConnection dbConnection = new DBConnection();
+            Connection connection = dbConnection.getConnection();
+
+            try {
+                PreparedStatement pst = connection.prepareStatement("select user_hashedpassword from qmusers where user_id=?");
+                pst.setString(1,login.getUserId());
+                ResultSet rs = pst.executeQuery();
+                if(rs.next()){
+                    String hashedPassword=rs.getString(1);
+                    if(checkHashedPassword.equals(hashedPassword)){
+                        System.out.println("LogIn Success :)");
+                    } else{
+                        System.out.println("Password doesnt match...." +
+                                "so Invalid User ID/Password");
+                    }
+                }
+                pst.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+
+
         }
 
+    }
+
+    private String userAvailable(){
+        DBConnection dbConnection = new DBConnection();
+        Connection con = dbConnection.getConnection();
+        boolean isAvailable=false;
+        String salt= "";    //empty string
+
+        try {
+            PreparedStatement pst = con.prepareStatement("select user_salt from qmusers where user_id=?");
+            pst.setString(1,login.getUserId());
+            ResultSet rs = pst.executeQuery();
+            if(rs.next()){
+                isAvailable=true;
+                salt=rs.getString(1);
+                System.out.println("Salt fetched from database: "+salt);    //for checking
+            } else{
+                System.out.println("User not available..." +
+                        "so User ID/Password is invalid");
+            }
+            pst.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return salt;
     }
 
 }
--- src/main/java/com/quizmaster/controller/SignupController.java
+++ src/main/java/com/quizmaster/controller/SignupController.java
@@ -1,14 +1,12 @@
 package com.quizmaster.controller;
 
 import com.quizmaster.database.DBConnection;
+import com.quizmaster.encryption.Hashing;
 import com.quizmaster.model.Signup;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 @ManagedBean(name = "signupcontroller")
 @RequestScoped
@@ -27,7 +25,65 @@ public class SignupController {
     }
 
     public void performSignup(){
+        if(signup.getUserPassword().equals(signup.getUserPasswordRepeat()) && !checkExistence()){
+            Hashing hashing = new Hashing();
+            //password encryption
+            String salt=hashing.generateRandomSalt();
+            String hashedPassword=hashing.performHash(signup.getUserPassword(),salt);
+            System.out.println("hashedPassword: "+hashedPassword);  //for checking
+            DBConnection dbconnection = new DBConnection();
+            Connection con = dbconnection.getConnection();
+            try{
+                //using prepared statement for value insertion in database
+                PreparedStatement pst = con.prepareStatement("insert into qmusers" +
+                        "(user_id,user_email,user_hashedpassword,user_salt) " +
+                        "values(?,?,?,?)");
+                pst.setString(1,signup.getUserId());
+                pst.setString(2,signup.getUserEmail());
+                pst.setString(3,hashedPassword);
+                pst.setString(4,salt);
+                pst.executeUpdate();
+                System.out.println("Data inserted..");  //for checking
+                pst.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            } finally {
+                try {
+                    //always gets executed
+                    con.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
 
+        }
+    }
+    private boolean checkExistence(){
+        DBConnection dbConnection = new DBConnection();
+        Connection con = dbConnection.getConnection();
+        PreparedStatement pst=null;
+        boolean isExist=false;
+        try {
+            pst = con.prepareStatement("select user_id from qmusers where user_id=? or user_email=?");
+            pst.setString(1,signup.getUserId());
+            pst.setString(2,signup.getUserEmail());
+            ResultSet rs = pst.executeQuery();
+            if(rs.next()){
+                isExist=true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            //safe to put inside finally
+            try {
+                if(pst!=null){pst.close();}
+                if(con!=null){con.close();}
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return isExist;
     }
 
 }
